cd5c06e84565a50da93f946fb62634a2e376fbf2
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-30 01:46:38 +0300

    feat:adds models

--- senditapp/models.py
+++ senditapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'images/')
+    location = models.CharField(max_length=255, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user
+  
+class Destination(models.Model):
+    name = models.CharField(max_length=100)  
+    
+    def __str__(self):
+        return self.name
+    
+class Parcel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    weighted = models.IntegerField()
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
