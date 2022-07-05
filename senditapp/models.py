e98468adfb96c2ec160a4111d6654263ef920b26
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-07-05 14:54:37 +0300

    feat:adds client and admin user models

--- senditapp/models.py
+++ senditapp/models.py
@@ -1,25 +1,44 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import AbstractUser
-
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
-class User(AbstractUser):
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    username = None
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+class User(AbstractUser):
+    is_staff = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        
+ 
+class Admin(models.Model):
+    user = models.OneToOneField(User, related_name='admin', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user.username)
+    
+    
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client')
+
+    def __str__(self):
+        return str(self.user.username)
 
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
     profile_pic = CloudinaryField('profile_pic', blank=True)
     location = models.CharField(max_length=255, null=False)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -29,15 +48,11 @@ class Profile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-  
     
 class Parcel(models.Model):
     name = models.CharField(max_length=80)
